99a808ba1086854c86357d56cec6c93d276aa38f
Tollan Renner <43834156+TollanUW@users.noreply.github.com>
2020-01-02 17:22:50 -0600

    Update FOMCcalendar.py

--- FOMCcalendar.py
+++ FOMCcalendar.py
@@ -34,13 +34,9 @@ def getFOMCtoday(fomcDates):
 def vCalcFOMCweek(fomcDays):
     return (fomcDays+1)//5
 
-def getFOMCweeks(mini = -6, maxi = 35):
+def getFOMCweeks(mini = -6, maxi = 34):
     weekRange = np.arange(mini, maxi)
     return weekRange[(weekRange+1)%5 == 0]
-
-getFOMCweeks()
-
-np.arange(-6,35)[(np.arange(-6,35)+1)%5 == 0]
 
 def loadReturns():
     # http://mba.tuck.dartmouth.edu/pages/faculty/ken.french/Data_Library/f-f_factors.html
@@ -68,7 +64,7 @@ def confidenceInterval(arr, confidence=0.95):
     h = std_err * t.ppf((1 + confidence) / 2, n - 1)
     return mu - h, mu, mu + h
 
-def getFOMCstats(fomcDates, dailyReturns = loadReturns(), days = 5, fomcDayRange = range(-6,35)):
+def getFOMCstats(fomcDates, dailyReturns = loadReturns(), days = 5, fomcDayRange = range(-6,34)):
     returnsDates = dailyReturns.index.values.astype('datetime64[D]')
     fomcDays = vCalcFOMCday(fomcDates, returnsDates)
     if days > 1:
@@ -82,36 +78,38 @@ def getFOMCstats(fomcDates, dailyReturns = loadReturns(), days = 5, fomcDayRange
     df.index.name = 'fomc_day'
     return df
 
-def getPlot(fomcDates, dailyReturns):
-    fomcData = getFOMCstats(fomcDates, dailyReturns)
+def getPlot(fomcDates, dailyReturns, days = 5):
+    fomcData = getFOMCstats(fomcDates, dailyReturns, days)
     source = ColumnDataSource(fomcData)
 
-    fig1 = figure(plot_width=1000)
-    fig1.title.text = "Stock returns over the FOMC cycle, {} onwards".format(dailyReturns.index[0].date())
-    fig1.patch(x=np.hstack((fomcData.index, fomcData.index[::-1])),y=np.hstack((fomcData['low'], fomcData['high'][::-1])), fill_color="#a6cee3")
-    fig1.line(x='fomc_day', y='mean', line_color='black', source=source)
-    fig1.circle(x='fomc_day', y='mean', color='black', source=source)
-    fig1.line(x='fomc_day', y='low', source=source)
-    fig1.line(x='fomc_day', y='high', source=source)
-    fig1.xaxis.axis_label = 'Days since FOMC meeting (weekends excluded)'
-    fig1.yaxis.axis_label = 'Avg. 5-day stock return, t to t+4'
+    p = figure(plot_width=1000)
+    p.title.text = "Stock returns over the FOMC cycle, {} onwards".format(dailyReturns.index[0].date())
+    p.xaxis.axis_label = 'Days since FOMC meeting (weekends excluded)'
+    p.yaxis.axis_label = 'Avg. 5-day stock return, t to t+4'
     labels = LabelSet(x='fomc_day', y='mean', text='fomc_day', x_offset=5, y_offset=-10, source=source, text_font_size='10pt')
-    fig1.add_layout(labels)
+    p.add_layout(labels)
+    
+    p.line(x='fomc_day', y='mean', line_color='black', source=source)
+    p.circle(x='fomc_day', y='mean', color='black', source=source)
+    confidenceBands = Band(base='fomc_day', lower='low', upper='high', source=source, level='underlay',
+      fill_alpha=1.0, line_width=1, line_color='black')
+    p.add_layout(confidenceBands)
     
     popMeanSpan = Span(location=dailyReturns.values.mean(), dimension='width', line_color='black', line_dash='dashed')
-    fig1.add_layout(popMeanSpan)
+    p.add_layout(popMeanSpan)
     zeroReturnSpan = Span(location=0, dimension='width')
-    fig1.add_layout(zeroReturnSpan)
+    p.add_layout(zeroReturnSpan)
     fomcTodaySpan = Span(location=int(getFOMCtoday(fomcDates)[2]), dimension='height', line_color='black', line_dash='dashed')
-    fig1.add_layout(fomcTodaySpan)
+    p.add_layout(fomcTodaySpan)
     
-    fig1.xgrid.ticker = FixedTicker(ticks=getFOMCweeks())
-    return fig1
+    p.xgrid.ticker = FixedTicker(ticks=getFOMCweeks())
+    return p
 
-def plotFOMC(fomcDates, dailyReturns = loadReturns(), lookback = 1000):
-    p = getPlot(fomcDates, dailyReturns[-lookback:])
+def plotFOMC(fomcDates, dailyReturns = loadReturns(), lookback = 10000, days = 5):
+    p = getPlot(fomcDates, dailyReturns[-lookback:], days)
     show(p)
 
 plotFOMC(fomcDates, dailyReturns)
-plotFOMC(fomcDates, dailyReturns, 10000)
+plotFOMC(fomcDates, dailyReturns, 10000, 1)
+plotFOMC(fomcDates, dailyReturns, 1000, 1)
 
